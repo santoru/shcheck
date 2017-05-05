544dfd249e7b3f33ec5e14ac14cac7ffd526eb84
m3liot <meliot@protonmail.com>
2017-05-05 12:35:25 +0200

    Added support for Information Disclosure Headers and Options

--- shcheck.py
+++ shcheck.py
@@ -3,6 +3,15 @@
 import urllib2
 import socket
 import sys
+from optparse import OptionParser
+
+parser = OptionParser("Usage: %prog [options] <target>", prog=sys.argv[0])
+parser.add_option("-p", "--port", dest="port",
+                  help="Set a custom port to connect to", metavar="PORT")
+parser.add_option("-i", "--information", dest="information", default=False,
+                  help="Display present information headers",
+                  action="store_true")
+(options, args) = parser.parse_args()
 
 
 class bcolors:
@@ -29,11 +38,11 @@ sec_headers = {
 
 }
 
+information_headers = {
+    'X-Powered-By',
+    'Server'
+}
 headers = {}
-
-
-def usage():
-    print "Usage: ./{} <target>".format(sys.argv[0])
 
 
 def banner():
@@ -66,10 +75,17 @@ def parse_headers(hdrs):
         headers.update({htype: hvalue})
 
 
+def append_port(target, port):
+    if target[-1:] == '/':
+        return target[:-1] + ':' + port + '/'
+    return target + ':' + port + '/'
+
+
 def check_target(target):
     '''
     Just put a protocol to a valid IP and check if connection works
     '''
+
     try:
         if (socket.inet_aton(target)):
             target = 'http://' + target
@@ -91,14 +107,7 @@ def check_https(target):
     '''
     Check if target support HTTPS for Strict-Transport-Security
     '''
-    t = target.replace('http://', 'https://')
-    try:
-        urllib2.urlopen(t, timeout=10)
-    except ValueError:
-        return False
-    except urllib2.URLError:
-        return False
-    return True
+    return target.startswith('https://')
 
 
 def report(target, safe, unsafe):
@@ -113,15 +122,24 @@ def report(target, safe, unsafe):
 def main(argv):
 
     if len(argv) < 2:
-        usage()
-        sys.exit(2)
+        parser.print_help()
+        sys.exit(1)
+
+    # Getting options
+    port = options.port
+    information = options.information
 
     banner()
     target = argv[1]
     safe = 0
     unsafe = 0
+
+    if port is not None:
+        target = append_port(target, port)
+
     # Check if target is valid
     response = check_target(target)
+    rUrl = response.geturl()
 
     print "[*] Analyzing headers of {}".format(colorize(target, 'info'))
     parse_headers(response.info().headers)
@@ -147,10 +165,24 @@ def main(argv):
             unsafe += 1
 
             # HSTS works obviously only on HTTPS
-            # if safeh == 'Strict-Transport-Security' and not check_https(target):
-            #    continue
+            if safeh == 'Strict-Transport-Security' and not check_https(rUrl):
+                unsafe -= 1
+                continue
             print '[!] Missing security header: {}'.format(
                 colorize(safeh, sec_headers.get(safeh)))
+
+    if information:
+        chk = False
+        print
+        for infoh in information_headers:
+            if infoh in headers:
+                chk = True
+                print "[!] Possible information disclosure: \
+header {} is present! (Value: {})".format(
+                        colorize(infoh, 'warning'),
+                        headers.get(infoh))
+        if not chk:
+            print "[*] No information disclosure headers detected"
 
     report(target, safe, unsafe)
 
