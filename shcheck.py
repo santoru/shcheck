745b5e522e46ffb23f5af445bb93aad325300673
m3liot <meliot@protonmail.com>
2017-06-02 14:57:58 +0200

    Added client headers

--- shcheck.py
+++ shcheck.py
@@ -36,6 +36,15 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+# Client headers to send to the server during the request.
+client_headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:53.0) Gecko/20100101 Firefox/53.0',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Language': 'en-US;q=0.8,en;q=0.3',
+    'Upgrade-Insecure-Requests': 1
+ }
+
+
 # Security headers that should be enabled
 sec_headers = {
     'X-XSS-Protection': 'warning',
@@ -110,7 +119,7 @@ def check_target(target, ssldisabled, useget):
         else:
             method = 'HEAD'
 
-        request = urllib2.Request(target)
+        request = urllib2.Request(target, headers=client_headers)
         request.get_method = lambda: method
 
         if ssldisabled:
