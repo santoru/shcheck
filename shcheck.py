6a21089ea0a682a4cb1e8c5703b9b228cb79a1c3
meliot <meliot@protonmail.com>
2019-07-28 17:44:27 +0200

    Updated copyright

--- shcheck.py
+++ shcheck.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # shcheck - Security headers check!
-# Copyright (C) 2018  meliot
+# Copyright (C) 2019  meliot
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
